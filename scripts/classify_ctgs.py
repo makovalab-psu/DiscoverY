e9ae4015468608ec07ab8a08776ed3e2ccc13135
Natasha Stopa <natasha.stopa@gmail.com>
2019-02-06 01:05:58 -0500

    clean up unused functions, add check for kmer file format

--- scripts/classify_ctgs.py
+++ scripts/classify_ctgs.py
@@ -7,12 +7,8 @@ from collections import defaultdict
 from numpy import median
 
 
-def classify_ctgs(kmer_size, bf, fem_kmers, mode):
-    male_contigs_file = "data/male_contigs.fasta"
-    reads_kmers = "data/kmers_from_male_reads"
-    # output
 
-    # make Bloom Filter of kmers from female
+def getbloomFilter(bf, fem_kmers, kmer_size):
     if bf:
         print("Opening Bloom Filter of k-mers from female")
         female_kmers_bf = BloomFilter.open("data/female.bloom")
@@ -23,14 +19,14 @@ def classify_ctgs(kmer_size, bf, fem_kmers, mode):
         bf_size = 3 * 1000 * 1000 * 1000
         bf_filename = "data/female.bloom"
         female_kmers_bf = BloomFilter(bf_size, .001, bf_filename)
-        
+
         if fem_kmers: # if female kmers file exist
             female_kmers_file = "data/female_kmers"
             with open(female_kmers_file, 'r') as fm_kmers:
                 for line in fm_kmers:
                     female_kmers_bf.add(line[:kmer_size])
         else :
-            female_reference_file = "data/female.fasta"    
+            female_reference_file = "data/female.fasta"
             for record in SeqIO.parse(female_reference_file,"fasta"):
                 to_kmerize_fwd = str(record.seq).upper()
                 length = len(to_kmerize_fwd)
@@ -38,22 +34,24 @@ def classify_ctgs(kmer_size, bf, fem_kmers, mode):
                     female_kmers_bf.add(to_kmerize_fwd[i:i+kmer_size])
 
         print("Done creating bloom filter")
+    return female_kmers_bf
 
+
+def classify_fm_male_mode(kmer_size, female_kmers_bf):
+    male_contigs_file = "data/male_contigs.fasta"
+    reads_kmers = "data/kmers_from_male_reads"
+
+    print("Generating a dictionary from kmers in kmers_from_male_reads")
+    kmer_abundance_dict_from_male = kmers.make_dict_from_kmer_abundance(reads_kmers, kmer_size)
 
     annotated_records = []
-    if mode == "female+male":
-            # make a dict of all kmers from male
-        print("Generating a dictionary from kmers in kmers_from_male_reads")
-        kmer_abundance_dict_from_male = defaultdict(int)
-        kmer_abundance_dict_from_male = kmers.make_dict_from_kmer_abundance(reads_kmers, kmer_size)
     ctg_count = 0
-           # for each contig in contigs.fasta, calculate proportion of kmers that are present in female data set
     n_kmers = "N"*kmer_size
+           # for each contig in contigs.fasta, calculate proportion of kmers that are present in female data set
     for record in SeqIO.parse(male_contigs_file, "fasta"):
         ctg_count += 1
         print("No. of contigs seen so far: ", ctg_count)
-        print("Current contig ID is : ", record.id)    
-        # convert the SeqRecord object's .seq into a string that needs to be kmerized
+        print("Current contig ID is : ", record.id)
         to_kmerize_fwd = str(record.seq).upper()
         length = len(to_kmerize_fwd)
         reverse = kmers.reverse_complement(to_kmerize_fwd)
@@ -71,35 +69,78 @@ def classify_ctgs(kmer_size, bf, fem_kmers, mode):
             # feature 1 : compute proportion of kmers shared with female
             if kmer not in female_kmers_bf and rev_kmer not in female_kmers_bf:
                 count_of_male_kmers_not_shared_with_female += 1
-            if mode=="female+male":
-                # feature 2 : find median of kmer abundances from reads
-                if kmer in kmer_abundance_dict_from_male :
-                    curr_kmer_abundances.append(kmer_abundance_dict_from_male[kmer])
-                elif rev_kmer in kmer_abundance_dict_from_male :
-                    curr_kmer_abundances.append(kmer_abundance_dict_from_male[rev_kmer])
-                else:
-                    continue
-        if mode=="female+male":        
-            if curr_kmer_abundances:
-                curr_median = median(curr_kmer_abundances)
-                print("Median is: ", curr_median)
+            # feature 2 : find median of kmer abundances from reads
+            if kmer in kmer_abundance_dict_from_male:
+                curr_kmer_abundances.append(kmer_abundance_dict_from_male[kmer])
+            elif rev_kmer in kmer_abundance_dict_from_male :
+                curr_kmer_abundances.append(kmer_abundance_dict_from_male[rev_kmer])
             else:
-                curr_median = 0
+                continue
+        if curr_kmer_abundances:
+            curr_median = median(curr_kmer_abundances)
+            print("Median is: ", curr_median)
+        else:
+            curr_median = 0
         print("Total No. of k-mers from this contig: ", count_of_male_kmers)
         print("No. of k-mers not shared with female: ", count_of_male_kmers_not_shared_with_female)
 
         if count_of_male_kmers != 0:
             proportion = count_of_male_kmers_not_shared_with_female / count_of_male_kmers
         else:
-            proportion = -1
+            proportion = 0
+        print("Proportion is: ", proportion)
+
+        annotated_records.append(SeqRecord(record.seq, id=record.id, description=str(length) + " " + str(proportion) + " " + str(curr_median)))
+    kmers.write_annotated_contigs_to_fasta(annotated_records)
+    return
+
+
+def classify_fm_mode(kmer_size, female_kmers_bf):
+    male_contigs_file = "data/male_contigs.fasta"
+    annotated_records = []
+
+    ctg_count = 0
+           # for each contig in contigs.fasta, calculate proportion of kmers that are present in female data set
+    for record in SeqIO.parse(male_contigs_file, "fasta"):
+        ctg_count += 1
+        print("No. of contigs seen so far: ", ctg_count)
+        print("Current contig ID is : ", record.id)
+
+        # convert the SeqRecord object's .seq into a string that needs to be kmerized
+        to_kmerize_fwd = str(record.seq).upper()
+        length = len(to_kmerize_fwd)
+        reverse = kmers.reverse_complement(to_kmerize_fwd)
+        count_of_male_kmers = 0
+        count_of_male_kmers_not_shared_with_female = 0
+        curr_kmer_abundances = []
+        for i in range(0, length-kmer_size+1):
+            kmer = to_kmerize_fwd[i:i+kmer_size]
+            rev_kmer = reverse[i:i+kmer_size]
+            count_of_male_kmers += 1
+            # feature 1 : compute proportion of kmers shared with female
+            if kmer not in female_kmers_bf and rev_kmer not in female_kmers_bf:
+                count_of_male_kmers_not_shared_with_female += 1
+
+        print("Total No. of k-mers from this contig: ", count_of_male_kmers)
+        print("No. of k-mers not shared with female: ", count_of_male_kmers_not_shared_with_female)
+
+        if count_of_male_kmers != 0:
+            proportion = count_of_male_kmers_not_shared_with_female / count_of_male_kmers
+        else:
+            proportion = 0
             print("Proportion is: ", proportion)
 
-        if mode=="female+male":
-            annotated_records.append(SeqRecord(record.seq, id=record.id, description=str(length) + " " + str(proportion) + " " + str(curr_median)))
-        else:
-            annotated_records.append(SeqRecord(record.seq, id=record.id, description=str(length) + " " + str(proportion)))
+        annotated_records.append(SeqRecord(record.seq, id=record.id, description=str(length) + " " + str(proportion)))
+    kmers.write_annotated_contigs_to_fasta(annotated_records)
+    return
 
-    with open("proportion_annotated_contigs.fasta", "w") as annotated_contigs_file:
-        SeqIO.write(annotated_records, annotated_contigs_file, "fasta") 
-    return 1
-    
+def classify_ctgs(kmer_size, bf, fem_kmers, mode):
+    female_kmers_bf = getbloomFilter(bf, fem_kmers, kmer_size)
+    if mode == "female+male":
+        classify_fm_male_mode(kmer_size, female_kmers_bf)
+    elif mode == "female_only":
+        classify_fm_mode(kmer_size, female_kmers_bf)
+    else:
+        print("Incorrect mode entered")
+        kmers.exit_gracefully()
+
--- scripts/kmers.py
+++ scripts/kmers.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from Bio import SeqIO
 from pybloomfilter import BloomFilter
-#from string import maketrans
 import sys
 
 
@@ -12,16 +11,6 @@ def exit_gracefully():
 def reverse_complement(seq):
     complement_map = str.maketrans("ACGTNacgtn", "TGCANtgcan")
     return seq[::-1].translate(complement_map)
-
-
-def kmerize(ip_string, kmer_size, step_size=1, offset=0):
-    """
-    function that kmerizes an input_string and returns a list of kmers
-    """
-    
-    return [str(ip_string[i:i + kmer_size]) for i in range(offset, len(ip_string) - kmer_size + 1, step_size)
-            if str(ip_string[i:i + kmer_size]) != "N"*kmer_size]
-    
 
 
 def make_bloom_from_kmer_abundance(ip_kmer_set, kmer_size, bf_size, bf_filename, ignore_rc=True):
@@ -38,57 +27,32 @@ def make_bloom_from_kmer_abundance(ip_kmer_set, kmer_size, bf_size, bf_filename,
     return bf
 
 
-# this one is for pybloom_live
-def legacy_make_bloom_from_kmer_abundance(ip_file, kmer_size, ignore_rc=True):
-    """
-    function that bloomifies a large file with kmers and count
-    file looks like : ATGCT 101
-    """
-    op_bloom = pybloom_live.ScalableBloomFilter(mode=pybloom_live.ScalableBloomFilter.SMALL_SET_GROWTH)
-    with open(ip_file, 'r') as kmers_fp:
-        for line in kmers_fp:
-            op_bloom.add(line[:kmer_size])
-            if ignore_rc == False:
-                op_bloom.add(reverse_complement(line[:kmer_size]))
-    return op_bloom
-
-
-def make_set_from_kmer_abundance(ip_file, kmer_size, ignore_rc=False):
-    """
-    function that settifies a large file with kmers and count
-    file looks like : ATGCT 101
-    """
-    kmers = set()
-    with open(ip_file, 'r') as kmers_fp:
-        for line in kmers_fp:
-            kmers.add(line[:kmer_size])
-            if ignore_rc == False :
-                kmers.add(reverse_complement(line[:kmer_size]))
-    return kmers
+def test_valid_kmer_format(sample_line, kmer_size):
+    sample_kmer_abun = sample_line.split(" ")
+    if len(sample_kmer_abun) != 2:
+        print("Format of kmer file is incorrect. Need a (kmer, count) pair separated by whitespace.")
+        exit_gracefully()
+    if len(sample_kmer_abun[0]) != kmer_size:
+        print("Format of kmer file does not match kmer size specified for DiscoverY")
+        exit_gracefully()
+    try:
+        int_test_line_kmer_abundance = int(sample_kmer_abun[1])
+    except ValueError:
+        print("Format of kmer file is incorrect. K-mer abundance in kmers_from_reads is not an int")
+        exit_gracefully()
 
 
 def make_dict_from_kmer_abundance (ip_file, kmer_size, ignore_rc=False):
     kmer_dicts = defaultdict(int)
-    with open(ip_file,'r') as file_handle1 :
-        for line in file_handle1 :
+    with open(ip_file,'r') as file_handle1:
+        first_line = file_handle1.readline()
+        test_valid_kmer_format(first_line, kmer_size)
+        for line in file_handle1:
             current_abundance = int(line.split(' ')[1])
             kmer_dicts[line[:kmer_size]] = current_abundance
             # kmer_dicts[reverse_complement(line[:kmer_size])] = current_abundance
     return kmer_dicts
 
-
-# function that uses BioPython to convert a fasta file to a dict
-def parse_ctgs_fasta (ip_fasta_file) :
-    with open(ip_fasta_file, "r") as handle :
-        contigs_dict = SeqIO.to_dict(SeqIO.parse(handle, "fasta"))
-    return contigs_dict
-
-
-def test_parse_ctgs () :
-    ip_fasta_file = "test/contigs.fasta"
-    contigs = parse_ctgs_fasta(ip_fasta_file)
-    for k, _ in contigs.iteritems() :
-        print(k, contigs[k].seq)
 
 
 # make a FASTA file with annotated contigs
